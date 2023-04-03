2818e66a3100c57e79f173e044b3766ae65b634f
Paulo Henryck <paulohenryck123@gmail.com>
2023-04-03 14:40:14 -0300

    Jornada Fullstack - Aula 06

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useEffect  } from "react";
 import VideoFooter from "./components/footer/VideoFooter";
 import VideoSidebar from "./components/sidebar/VideoSidebar";
 import "./video.css";
@@ -16,6 +16,24 @@ function Video({likes,messages,shares,name,description,music,url}) {
       setPlay(true);
     }
   }
+
+  useEffect(() => {
+    const handleScroll = () => {
+        if (videoRef.current && play) {
+            videoRef.current.currentTime = 0;
+            videoRef.current.pause();
+            setPlay(false);
+        }
+        };
+    
+        window.addEventListener("keydown", handleScroll);
+        window.addEventListener("wheel", handleScroll);
+    
+        return () => {
+        window.removeEventListener("keydown", handleScroll);
+        window.removeEventListener("wheel", handleScroll);
+        };
+    }, [play]);
 
   return (
     <div className="video">
